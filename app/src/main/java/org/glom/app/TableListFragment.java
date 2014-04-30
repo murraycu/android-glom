48058f6ccdd529609114c6cf7e25915221b72a91
Murray Cumming <murrayc@murrayc.com>
2014-04-30 12:37:57 +0200

    TableListFragment: Fix a typo to use the correct UI resource.

--- app/src/main/java/org/glom/app/TableListFragment.java
+++ app/src/main/java/org/glom/app/TableListFragment.java
@@ -36,7 +36,7 @@ public class TableListFragment extends TableDataFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_table_detail, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_table_list, container, false);
         assert rootView != null;
 
         // Show the dummy content as text in a TextView.
