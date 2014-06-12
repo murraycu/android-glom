0fe1111a2bf92b7d4734a9aba226e7f73f760b7a
Murray Cumming <murrayc@murrayc.com>
2014-06-12 13:53:34 +0200

    Add copyright/license blocks to test and UI source code.

--- app/src/main/java/org/glom/app/TableListFragment.java
+++ app/src/main/java/org/glom/app/TableListFragment.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2014 Murray Cumming
+ *
+ * This file is part of android-glom.
+ *
+ * android-glom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * android-glom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with android-glom.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.glom.app;
 
 import android.app.Activity;
